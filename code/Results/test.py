5eb67aaa97988b7deaa06485a1dc8ef7dab1cd78
Bryan Pan <bryanpan342@gmail.com>
2019-10-20 23:00:25 -0700

    init repo

---
+++ code/Results/test.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+data_sift = np.load('voc_sift_kmeans_20.npy')
+data_surf = np.load('voc_surf_kmeans_20.npy')
+data_orb = np.load('voc_orb_kmeans_20.npy')
+print(data_sift)
+print(data_surf)
+print(data_orb)
